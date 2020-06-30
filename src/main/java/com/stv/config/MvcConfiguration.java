d772f7aedc4523229675a05fd514e7a8c79b4cd4
Nguyen Luc <lucnv1@vnext.com.vn>
2020-06-30 09:52:51 +0900

    no message

--- src/main/java/com/stv/config/MvcConfiguration.java
+++ src/main/java/com/stv/config/MvcConfiguration.java
@@ -1,19 +1,37 @@
 package com.stv.config;
 
+import javax.sql.DataSource;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
+import com.stv.dao.TaskDAOImpl;
+import com.stv.dao.TaskInfoDAO;
+
 @Configuration
 @ComponentScan(basePackages="com.stv")
 @EnableWebMvc
 public class MvcConfiguration implements WebMvcConfigurer{
 
+	@Bean
+    public DataSource getDataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/sampledb");
+        dataSource.setUsername("root");
+        dataSource.setPassword("nguyenluc1@");
+         
+        return dataSource;
+    }
+	
+	
 	@Bean
 	public ViewResolver getViewResolver(){
 		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
@@ -26,6 +44,10 @@ public class MvcConfiguration implements WebMvcConfigurer{
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
 	}
-
 	
+     
+    @Bean
+    public TaskInfoDAO getTaskInfoDAO() {
+        return new TaskDAOImpl(getDataSource());
+    }
 }
